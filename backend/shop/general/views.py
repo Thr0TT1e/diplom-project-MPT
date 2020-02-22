a4656755375e7dc977b6f2ab2c20c8eaeb28335e
ThreeT <info@noble24.pro>
2020-02-22 23:43:41 +0300

    Подправил представления (views.py)

--- backend/shop/general/views.py
+++ backend/shop/general/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def index(request):
-	return render(request, 'novakids/index.html')
+	return render(request, 'general/index.html', {})
--- backend/shop/shop/urls.py
+++ backend/shop/shop/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('general/', include('general.urls')),
+    path('', include('general.urls')),
     # path('', RedirectView.as_view(url='/general/', permanent=True)),
 ]
