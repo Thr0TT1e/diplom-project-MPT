c67c63429d7e6937a769f874df4e8bd219565e86
ThreeT <info@noble24.pro>
2020-02-22 17:19:38 +0300

    Добавлен класс "Карточка продукта" в модель (models.py), а также методы публикации и возврата названия товара. Добавлена запись в админку (admin.py).

--- backend/shop/general/models.py
+++ backend/shop/general/models.py
@@ -9,24 +9,36 @@ class - ключевое слово для определения объекто
 """
 
 from django.db import models
+from django.utils import timezone
 
 
 class CardProduct(models.Model):
-	SHIRT_SIZES = (
+	SHIRT_SIZES = [
 		('3T', '3 года'),
 		('4T', '4 года'),
 		('5T', '5 лет'),
 		('6T', '6 лет'),
 		('7T', '7 лет'),
-	)
-	GENDER = (
+	]
+	GENDER = [
 		('М', 'Мальчик'),
 		('Д', 'Девочка'),
-	)
+	]
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	price = models.IntegerField()
-	gender = models.CharField(max_length=1, choices=GENDER)
+	gender = models.CharField(max_length=2, choices=GENDER)
 	image = models.ImageField(upload_to='%Y/%m/%d/')
-	size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+	size = models.CharField(max_length=2, choices=SHIRT_SIZES)
 	date_publish = models.DateField(auto_now_add=True)
+
+	class Meta:
+		verbose_name = 'Карточка продукта'
+		verbose_name_plural = 'Карточка продукта'
+
+	def publish(self):
+		self.date_publish = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
