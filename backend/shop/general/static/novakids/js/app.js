9828046f73f8b3c0ab89b0ce95fb9914a450aefc
ThreeT <info@noble24.pro>
2020-02-28 10:56:11 +0300

    Организована структура шаблона ИМ, созданы необходимые страницы и настроена маршрутизация между ними, плюс статика.

---
+++ backend/shop/general/static/novakids/js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    $(document).ready(function () {
+        $('#slider_img-inner').slick({
+            accessibility: true,
+            speed: 300,
+            autoplay: true,
+            arrows: false,
+            dots: true,
+            adaptiveHeight: true,
+        });
+    });
+
+});
